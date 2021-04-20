94b897a8f39bc28d615eb3f7ee24ff74449a3569
André Felipe <andre88444@gmail.com>
2021-04-20 18:47:45 -0300

    Atualização

--- src/Comentario_routes.md
+++ src/Comentario_routes.md
@@ -2,10 +2,25 @@ import { Router } from "express";
 
 import { getCustomRepository } from "typeorm";
 
-import { SettingsRepository } from "./repositories/SettingsRepository";
+import { SettingsRepository } from "./database/repositories/SettingsRepository";
 
 
 const routes = Router();
+
+/**
+ ** Tipos de parâmetros
+ * 
+ * Routes Params => Parâmetros de rotas 
+ *-- http://localhost:3333/settings/1
+ * 
+ * Query Params => Filtros e buscas
+ *-- http://localhost:3333/settings/1?search=algumacoisa
+ * 
+ * Body params => Objetos e Array
+ *-- {[Objeto],[Objetos]}
+ *
+ */
+
 
 routes.post("/settings", async (request, response) => {
     const { chat, username } = request.body;
--- src/server.ts
+++ src/server.ts
@@ -1,40 +1,11 @@
 import express from "express";
 
 import "./database";
-import { routes } from "./routes";
 
-// import { router } from "./routes"
+import { routes } from "./routes";
 
 const app = express();
 const port = 3333
-
-
-
-/**
- * GET = Buscas
- * POST = Criação
- * PUt = Alteração
- * DELETE = DELETAR
- * PATCH = Altera uma informação especifica
- */
-
-// app.get("/", (request, response) => { // "request" Tudo o que ta vindo "response" tudo o que vai retona para o usuario
-//     // return response.send("ola")// retona para a pagina body > ola
-
-//     return response.json({ // retona para a pagina body > {"message":"ola"}
-//         message: "ola"
-//     });
-// });
-
-
-// app.post("/", (request, responses) => {
-
-//     return responses.json({ message: "Usuário salvo com sucesso" })
-
-// });
-
-
-
 
 app.use(express.json())
 
