332642e138cf1b70ef4cd05645e40e131144dce0
André Felipe <andre88444@gmail.com>
2021-04-21 11:10:13 -0300

    Atualização

--- src/controllers/SettingsController.ts
+++ src/controllers/SettingsController.ts
@@ -1,20 +1,23 @@
 import { Request, Response } from 'express';
-import { getCustomRepository } from 'typeorm';
-import { SettingsRepository } from '../repositories/SettingsRepository';
+
+import { SettingsService } from '../services/SettingsService';
 class SettingsController {
-    async create(req: Request, res: Response) {
-        const { chat, username } = req.body;
+    async create(request: Request, response: Response) {
+        const { chat, username } = request.body;
 
-        const settingsRepository = getCustomRepository(SettingsRepository);
+        const settingsService = new SettingsService();
 
-        const setting = settingsRepository.create({
-            chat,
-            username,
-        });
+        try {
+            const settings = await settingsService.create({ chat, username });
 
-        await settingsRepository.save(setting);
 
-        return res.json(setting);
+            return response.json(settings);
+        } catch (err) {
+            return response.status(400).json({
+                message: err.message,
+            })
+        }
+
     }
 }
 
---
+++ src/services/SettingsService.ts
@@ -0,0 +1,35 @@
+import { getCustomRepository } from "typeorm";
+import { SettingsRepository } from "../repositories/SettingsRepository";
+
+
+interface ISettingsCreate {
+    chat: boolean;
+    username: string;
+
+}
+
+class SettingsService {
+    async create({ chat, username }) {
+        const settingsRepository = getCustomRepository(SettingsRepository);
+
+        const userAlreadyExists = await settingsRepository.findOne({
+            username
+        });
+
+        if (userAlreadyExists) {
+            throw new Error("O usuário ja existe!")
+        }
+
+        const setting = settingsRepository.create({
+            chat,
+            username,
+        });
+
+        await settingsRepository.save(setting);
+
+        return setting;
+
+    }
+}
+
+export { SettingsService };
