1aa434e284c158d8285026f47d64ca7d1f15d451
Your Name Comes Here <andre88444@gmail.com>
2021-04-22 16:34:24 -0300

    Intalaçao do public e configuração

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,43 @@
 import express from "express";
-import { routes } from "./routes";
+import { createServer } from "http";
+import { Server, Socket } from "socket.io";
+import path from "path";
+
 import "./database";
+import { routes } from "./routes";
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, "..", "public")));
+app.set("views", path.join(__dirname, "..", "public"));
+app.engine("html", require("ejs").renderFile);
+app.set("view engine", "html");
+
+app.get("/pages/client", (request, response) => {
+  return response.render("html/client.html");
+})
+
+
+const http = createServer(app)
+const io = new Server(http)
+
+io.on("connection", (socket: Socket) => {
+  console.log("Se conectou", socket.id)
+});
+
 const port = 3333;
 
 app.use(express.json());
 
 app.use(routes);
 
-app.listen(port, () => console.log(`O servidor esta rodando acesse: localhost:${port}`))
+http.listen(port, () => console.log(log(port)))
+
+
+
+
+function log(port: number): string {
+  return `O servidor esta rodando acesse: localhost:${port}`
+}
+
+
